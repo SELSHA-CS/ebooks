03c0361da62d5fc9d3e89caef43cb1f4f9ee061f
SELSHA-CS <selshacs7@gmail.com>
2024-01-30 15:32:31 +0530

    commit

--- lib/firebase_database/firebase_crud/storagemain.dart
+++ lib/firebase_database/firebase_crud/storagemain.dart
@@ -46,6 +46,39 @@ class _FireBaseCRUDState extends State<FireBaseCRUD> {
         title: Text("User Data"),
         backgroundColor: Colors.green,
       ),
+      body: StreamBuilder<QuerySnapshot>(stream: readUser(), builder: (context, snapshot){
+        if(snapshot.hasError){
+          return Center(child: Text("Error ${snapshot.error}"));
+        }
+        if(snapshot.connectionState == ConnectionState.waiting){
+          return Center(child: CircularProgressIndicator());
+        }
+        final users = snapshot.data!.docs;
+        return ListView.builder(
+          itemCount: users.length,
+          itemBuilder: ((context, index) {
+          //final users = snapshot.data!.docs[index];
+          final user = users[index]; 
+          final userId = user.id;
+          final userName = user["name"]; // or final uerName = user[index]["name"];
+          final userEmail = user["email"];
+
+          return ListTile(
+            title: Text("$userName", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
+            subtitle: Text("$userEmail", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),),
+            trailing: Wrap(
+              children: [
+                IconButton(onPressed: (){
+                  editUserData(userId);
+                }, icon: Icon(Icons.edit)),
+                IconButton(onPressed: (){
+                  deleteUser(userId);
+                }, icon: Icon(Icons.delete)),
+              ],
+            ),
+          );
+        }));
+      }),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.green,
         onPressed: () => createUser(),
@@ -109,6 +142,67 @@ class _FireBaseCRUDState extends State<FireBaseCRUD> {
       Navigator.of(context).pop();
     }).catchError((error){
       print("Failed to add data $error");
+    });
+  }
+  
+  ///read all the documents through querysnapshot
+  Stream<QuerySnapshot> readUser() {
+    return _userCollection.snapshots();
+  }
+  
+  ///show bottomsheet to edit userdata
+  void editUserData(String userId) {
+    var uname  = TextEditingController();
+    var uemail = TextEditingController();
+    showModalBottomSheet(context: context, builder: (context){
+      return Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: uname,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Name",
+              ),
+            ),
+            SizedBox(height: 10,),
+            TextField(
+              controller: uemail,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Email",
+              ),
+            ),
+            SizedBox(height: 10,),
+            ElevatedButton(onPressed: (){
+              updateUser(userId, uname.text, uemail.text);
+              uname.clear();
+              uemail.clear();
+            }, child: Text("Update User"))
+          ],
+        ),
+      );
+    });
+  }
+  
+  Future<void> updateUser(String userId, String uname, String uemail) async{
+    var updatevalues = {"name":uname, "email":uemail};
+    return _userCollection.doc(userId).update(updatevalues).then((value){
+      print("User data updated succesffully");
+    }).catchError((error){
+      print("User data updating failed");
+    });
+  }
+  
+  Future<void> deleteUser(var id) async{
+    return _userCollection.doc(id).delete().then((value) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("User Deleted Successfully"))
+      );
+    }).catchError((error){
+      print("User deletion failed $error");
     });
   }
 }
